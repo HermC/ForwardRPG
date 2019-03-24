34bebc83ea2a7aa012db27dc82fd0d14cf7c063f
finominon <zcj14@software.nju.edu.cn>
2019-03-24 23:58:11 +0800

    auto fight

--- src/main/java/edu/nju/hermc/forward/game/creature/Enemy.java
+++ src/main/java/edu/nju/hermc/forward/game/creature/Enemy.java
@@ -1,6 +1,10 @@
 package edu.nju.hermc.forward.game.creature;
 
+import edu.nju.hermc.forward.game.skill.Skill;
 import edu.nju.hermc.forward.game.skill.bag.Bag;
+import edu.nju.hermc.forward.game.skill.warrior.Attack;
+
+import java.util.ArrayList;
 
 public class Enemy extends Creature {
 
@@ -8,6 +12,11 @@ public class Enemy extends Creature {
 
     public Enemy(String objectId, int hp, int mp, int ap, int level, Bag bag) {
         super(objectId, hp, mp, ap, level, bag);
+        int skilllevel  = level / 10;
+        ArrayList<Skill> skillList = new ArrayList<>();
+        Attack attack0 = new Attack(skilllevel);
+        skillList.add(attack0);
+        this.skillList = skillList;
     }
 
 
